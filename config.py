72eb17d8808d3dd6c8e088f82be9287320634912
abdol <a@fkrtech.com>
2024-03-18 03:19:04 +0000

    ✨ Add two plots with filtering

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ UNITS = {
         "co2_penalty": "M (USD)"
 }      
 KPIS = {
-    "wacc": "WACC",
-    "scores": "Scores",
-    "factory_utilization": "Factory Utilization",
-    "employee_engagement": "Employee Engagement",
-    "interest_coverage": "Interest Coverage",
-    "marketing_spend_rev": "Cumulative Marketing Spend/Rev",
-    "e_cars_sales": "eCars Sales",
-    "co2_penalty": "CO2 Penalty"
+    "wacc": ("WACC", "#CC6677"),
+    "scores": ("Scores", "#332288"),
+    "factory_utilization": ("Factory Utilization", "#DDCC77"),
+    "employee_engagement": ("Employee Engagement", "#117733"),
+    "interest_coverage": ("Interest Coverage", "#88CCEE"),
+    "marketing_spend_rev": ("Cumulative Marketing Spend/Rev", "#882255"), 
+    "e_cars_sales": ("eCars Sales", "#44AA99"),
+    "co2_penalty": ("CO2 Penalty", "#999933"),
 } 
 TEAM_NAMES = [
     "fovro", 
@@ -80,3 +80,13 @@ TEAM_NAMES = [
 TEAMS = {}
 for i in range(len(TEAM_NAMES)):
     TEAMS[i + 1] = TEAM_NAMES[i]
+
+MAIN_PLOT_WIDTH = 1000
+MAIN_PLOT_HEIGHT = 600
+MAIN_PLOT_TITLE = "KPIs by Team"
+SIDE_PLOT_WIDTH = 600
+SIDE_PLOT_HEIGHT = 600
+SIDE_PLOT_TITLE = "Percentage KPIs"
+APP_TITLE = "<h1>Business in Practice | KPI Data Visualiser</h1>"
+LEGEND_TITLE = "KPI (click to mute)"
+PLOT_BACKGROUND_COLOR = "#EEEEEE"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # IMPORTS
-from bokeh.layouts import row
+import datetime
+from bokeh.layouts import row, column, layout
 from bokeh.plotting import figure, show, curdoc
-from bokeh.models import ColumnDataSource, DateSlider
+from bokeh.models import ColumnDataSource, DateSlider, MultiChoice, Legend
+from bokeh.models.widgets import Div
 from config import *
 import json 
 import os
@@ -18,7 +20,7 @@ for file in sorted(os.listdir(DATA_DIR)):
 
 # DATA PRE-PROCESSING
 dfs = {}
-for date, json_data in data.items():
+for _date, json_data in data.items():
     df = pd.DataFrame(json_data)
     for col in df.columns:
         df[col] = df[col].sort_values(key=lambda x: x.apply(lambda x: x['team']), ignore_index=True)
@@ -26,34 +28,98 @@ for date, json_data in data.items():
         df[col] = df[col].apply(lambda x: x['value'])
     df.set_index("team_id", inplace=True)
     df = df.apply(pd.to_numeric)
-    dfs[date] = df
-    
-    print("Pre-processed JSON data for date: ", date)
+    dfs[_date] = df
+    print("Pre-processed JSON data for date: ", _date)
+
 
 # DATA VISUALIZATION
-dates = np.array(list(dfs.keys()), dtype=np.datetime64)
-source = ColumnDataSource(data=dict(date=dates, team_id= dfs[date].index, scores=dfs[date]["scores"]))
+## DESCRIPTION
+description = Div(text=APP_TITLE)
 
-score_plot = figure(x_range=TEAM_NAMES, title="Scores by Team")
-score_plot.scatter(x='team_id', y='scores', source=source, width=0.9)
-score_plot.xgrid.grid_line_color = None
-score_plot.y_range.start = 0
-score_plot.xaxis.major_label_orientation = 1
+## FUNCTIONS
+def update_source(date, exclude_kpis = []):
+    source_dict = dict(
+        date=dates, 
+        team_id= dfs[date].index, 
+        scores=dfs[date]["scores"],
+        wacc=dfs[date]["wacc"], 
+        factory_utilization=dfs[date]["factory_utilization"],
+        employee_engagement=dfs[date]["employee_engagement"],
+        interest_coverage=dfs[date]["interest_coverage"],
+        marketing_spend_rev=dfs[date]["marketing_spend_rev"],
+        e_cars_sales=dfs[date]["e_cars_sales"],
+        co2_penalty=dfs[date]["co2_penalty"],
+    )
+    for kpi in exclude_kpis:
+        source_dict[kpi] = np.zeros(len(TEAM_NAMES))
+    return source_dict
 
-# Create a slider: slider
 def update_plot(attr, old, new):
-    date_value = slider.value_as_date.strftime('%Y-%m-%d')
-    print('Slider value: ', slider.value, date_value)
-    source.data = dfs[date_value]
+    print('Slider value: ', slider.value, '- selected KPIs: ', kpi_selector.value)
+    date_value = pd.to_datetime(slider.value_as_date).strftime('%Y-%m-%d')
+    kpi_values = kpi_selector.value
+    print('Slider value: ', date_value, '- selected KPIs: ', kpi_values)
+    if date_value in dfs:
+        exclude_kpis = [kpi for kpi in KPIS if kpi not in kpi_values]
+        source_dict = update_source(date_value, exclude_kpis)
+        source.data = source_dict
+    else:
+        print("No data for date: ", date_value)
+        zeros = np.zeros(len(TEAM_NAMES))
+        source.data = dict(date=dates, team_id=TEAM_NAMES, scores=zeros)
+
+## PLOT SETUP
+dates = np.array(list(dfs.keys()), dtype=np.datetime64)
+date = list(dfs.keys())[0]
+source = ColumnDataSource(data=update_source(date))
+
+## CONTROLS
 slider = DateSlider(title='Date', start=dates[0], end=dates[-1], step=1, value=dates[0])
 slider.on_change('value', update_plot)
+kpi_selector = MultiChoice(title="KPIs", value=list(KPIS.keys()), options=list(KPIS.keys()), solid=False)
+kpi_selector.on_change('value', update_plot)
+
+## PLOTS
+main_legend = []
+side_legend = []
+kpi_plot = figure(width=MAIN_PLOT_WIDTH, height=MAIN_PLOT_HEIGHT, x_range=TEAM_NAMES, title=MAIN_PLOT_TITLE)
+for kpi in KPIS:
+    color = KPIS[kpi][1]
+    c1 = kpi_plot.line(x='team_id', y=kpi, source=source, color=color, alpha=0.8, line_width=4)
+    c2 = kpi_plot.scatter(x='team_id', y=kpi, source=source, color=color, size=8)
+    main_legend.append((KPIS[kpi][0], [c1, c2]))
+
+kpi_plot.background_fill_color = PLOT_BACKGROUND_COLOR
+kpi_plot.y_range.start = 0
+kpi_plot.xaxis.major_label_orientation = 1
+kpi_legend = Legend(items=main_legend)
+kpi_plot.add_layout(kpi_legend, 'right')
+kpi_legend.title = LEGEND_TITLE
+kpi_legend.click_policy="mute"
+
+percentage_kpis = ["wacc", "factory_utilization", "employee_engagement", "interest_coverage"]
+percentage_kpi_plot = figure(width=SIDE_PLOT_WIDTH, height=SIDE_PLOT_HEIGHT, x_range=TEAM_NAMES, title=SIDE_PLOT_TITLE)
+for kpi in percentage_kpis:
+    color = KPIS[kpi][1]
+    c1 = percentage_kpi_plot.line(x='team_id', y=kpi, source=source, color=color, alpha=0.8, width=0.8, line_width=4)
+    c2 = percentage_kpi_plot.scatter(x='team_id', y=kpi, source=source, color=color, size=8)
+    side_legend.append((KPIS[kpi][0], [c1, c2]))
+
+percentage_kpi_plot.background_fill_color = PLOT_BACKGROUND_COLOR
+percentage_kpi_plot.y_range.start = 0
+percentage_kpi_plot.xaxis.major_label_orientation = 1
+percentage_kpi_legend = Legend(items=side_legend)
+percentage_kpi_plot.add_layout(percentage_kpi_legend, 'right')
+percentage_kpi_legend.title = LEGEND_TITLE
+percentage_kpi_legend.click_policy="mute"
 
 
-# wacc_plot = figure(x_range=TEAM_NAMES, title="WACC by Team")
-# wacc_plot.vbar(x=TEAM_NAMES, top=dfs["2023-06-19"]["wacc"], width=0.9)
-# wacc_plot.xgrid.grid_line_color = None
-# wacc_plot.y_range.start = 0
-# score_plot.xaxis.major_label_orientation = 1
-curdoc().add_root((row(score_plot, slider)))
+# LAYOUT
+layout = layout([
+    row([description], sizing_mode='fixed'),
+    row([slider, kpi_selector], sizing_mode='fixed'),
+    [kpi_plot],
+    [percentage_kpi_plot],
+], sizing_mode='stretch_both')
 
-    
+curdoc().add_root(layout)
